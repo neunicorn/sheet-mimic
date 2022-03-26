4e50167c7b7c7a4c9536fceac2237db65d940a1e
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2022-03-26 15:32:43 +0700

    merapihkan memberi id dan number

--- js/interactive.js
+++ js/interactive.js
@@ -1,36 +1,52 @@
-    const jendela = window;
-    jendela.onkeydown = function(event){
-        if(event.keyCode == 9){
-            console.log("tab was pressed");
-            const tableBody = document.getElementById("tableBody");
+    let key = [];
 
+    let number = 2;
+
+    function addRow(){
+        console.log("tab was pressed");
+            
+
+
+            const tableBody = document.getElementById("tableBody");
             const tr = document.createElement("tr");
             const td = document.createElement("td");
+            td.setAttribute("id", number);
+            td.textContent = number;
             const td2 = document.createElement("td");
             const td3 = document.createElement("td");
-            const input = document.createElement("input");
-            input.setAttribute("type", "text");
+            const td4 = document.createElement("td");
+
+        
             const input2 = document.createElement("input");
             input2.setAttribute("type", "text");
+            input2.setAttribute("onclick", "this.select()");
+
             const input3 = document.createElement("input");
             input3.setAttribute("type", "text");
+            input3.setAttribute("onclick", "this.select()");
+
+            const input4 = document.createElement("input");
+            input4.setAttribute("type", "text");
+            input4.setAttribute("onclick", "this.select()");
             
-           td.appendChild(input);
            td2.appendChild(input2);
            td3.appendChild(input3);
+           td4.appendChild(input4);
+
            tr.appendChild(td);
            tr.appendChild(td2);
            tr.appendChild(td3);
-            // for (let index = 0; index < 3; index++) {
-            //     td.appendChild(input);
-            //     tr.appendChild(td);
-                
-            // }
+           tr.appendChild(td4);
     
             tableBody.appendChild(tr);
+            number++;
+    }
+
+    const jendela = window;
+    const badan = document.getElementById("thisisbody");
+    badan.onkeydown = function(event){
+        if(event.keyCode == 9){
+            addRow();
         }
         
     }
-    
-
-
